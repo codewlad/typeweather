03ab3b2e0d8c2c0c1ff0a7156c34e866a4d06016
Wladimir Ferreira Gonçalves <codewlad@gmail.com>
2024-01-06 22:59:52 -0300

    feat: Add Typescript support

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,8 @@ import ReactDOM from 'react-dom/client';
 
 import { Routes } from './routes';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Routes />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+	<React.StrictMode>
+		<Routes />
+	</React.StrictMode>
+);
